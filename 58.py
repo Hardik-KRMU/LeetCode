cacb4bcc2cf81d6def150da4921fd4d3bcbd2a9f
Hardik-KRMU <hardikchandra713@gmail.com>
2026-04-21 22:45:48 +0530

    first commit

---
+++ 58.py
@@ -0,0 +1,13 @@
+class Solution:
+    def lengthOfLastWord(self, s):
+        i = len(s) - 1
+        
+        while s[i] == ' ':
+            i -= 1
+        
+        j = i
+        
+        while j >= 0 and s[j] != ' ':
+            j -= 1
+        
+        return i - j
