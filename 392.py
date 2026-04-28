9d3b5c492b52e194ec5969aee58d56e7ff097091
Hardik-KRMU <hardikchandra713@gmail.com>
2026-04-28 23:58:25 +0530

    first commit

--- 392.py
+++ 392.py
@@ -1,10 +1,11 @@
 class Solution:
     def isSubsequence(self, s, t):
-        i = j = 0
-
-        while i < len(s) and j < len(t):
-            if s[i] == t[j]:
-                i += 1
-            j += 1
-
-        return i == len(s)
+        if not s:
+            return True
+        if not t:
+            return False
+        
+        if s[0] == t[0]:
+            return self.isSubsequence(s[1:], t[1:])
+        
+        return self.isSubsequence(s, t[1:])
