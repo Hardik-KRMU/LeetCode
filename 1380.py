627e5d5dd61fa919df3d5323f30ce37f86e53451
Hardik-KRMU <hardikchandra713@gmail.com>
2026-04-28 23:47:50 +0530

    first commit

--- 1380.py
+++ 1380.py
@@ -1,5 +1,12 @@
+<<<<<<< HEAD
 class Solution:
     def luckyNumbers(self, matrix):
         rows = {min(row) for row in matrix}
         cols = {max(col) for col in zip(*matrix)}
+=======
+class Solution:
+    def luckyNumbers(self, matrix):
+        rows = {min(row) for row in matrix}
+        cols = {max(col) for col in zip(*matrix)}
+>>>>>>> 0120bca (first commit)
         return list(rows & cols)
