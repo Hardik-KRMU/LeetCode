0629805d01c382cdf147942b208f10711c534632
Hardik-KRMU <hardikchandra713@gmail.com>
2026-04-30 23:32:23 +0530

    first commit

--- 162.py
+++ 162.py
@@ -1,13 +1,6 @@
 class Solution:
     def findPeakElement(self, nums):
-        l, r = 0, len(nums) - 1
-
-        while l < r:
-            mid = (l + r) // 2
-
-            if nums[mid] < nums[mid + 1]:
-                l = mid + 1
-            else:
-                r = mid
-
-        return l
+        for i in range(len(nums)-1):
+            if nums[i] > nums[i+1]:
+                return i
+        return len(nums)-1
