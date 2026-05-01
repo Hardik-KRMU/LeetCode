30990a45e508da162be703fbdbdfc032449a2e22
Hardik-KRMU <hardikchandra713@gmail.com>
2026-05-01 23:17:58 +0530

    first commit

--- 36.py
+++ 36.py
@@ -1,14 +1,22 @@
 class Solution:
     def isValidSudoku(self, board):
-        seen = set()
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        boxes = [set() for _ in range(9)]
 
         for r in range(9):
             for c in range(9):
                 val = board[r][c]
-                if val != '.':
-                    if (val, r) in seen or (c, val) in seen or (r//3, c//3, val) in seen:
-                        return False
-                    seen.add((val, r))
-                    seen.add((c, val))
-                    seen.add((r//3, c//3, val))
+                if val == '.':
+                    continue
+
+                b = (r // 3) * 3 + (c // 3)
+
+                if val in rows[r] or val in cols[c] or val in boxes[b]:
+                    return False
+
+                rows[r].add(val)
+                cols[c].add(val)
+                boxes[b].add(val)
+
         return True
