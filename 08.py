dafd1104812380ec61150c4dabf365b6397aa459
Hardik-KRMU <hardikchandra713@gmail.com>
2026-05-04 22:59:10 +0530

    first commit

--- 08.py
+++ 08.py
@@ -1,31 +1,28 @@
 class Solution:
     def myAtoi(self, s):
-        i = 0
-        n = len(s)
+        s = s.strip()
+        if not s:
+            return 0
 
-        # 1. skip spaces
-        while i < n and s[i] == ' ':
-            i += 1
-
-        # 2. sign
         sign = 1
-        if i < n and (s[i] == '+' or s[i] == '-'):
-            sign = -1 if s[i] == '-' else 1
+        i = 0
+
+        if s[0] in '+-':
+            sign = -1 if s[0] == '-' else 1
             i += 1
 
-        # 3. convert digits
         res = 0
         INT_MAX = 2**31 - 1
         INT_MIN = -2**31
 
-        while i < n and s[i].isdigit():
-            digit = int(s[i])
+        while i < len(s) and s[i].isdigit():
+            res = res * 10 + int(s[i])
 
-            # 4. overflow check
-            if res > (INT_MAX - digit) // 10:
-                return INT_MAX if sign == 1 else INT_MIN
+            if sign * res >= INT_MAX:
+                return INT_MAX
+            if sign * res <= INT_MIN:
+                return INT_MIN
 
-            res = res * 10 + digit
             i += 1
 
         return sign * res
