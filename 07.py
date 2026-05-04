39c5ba08275fa2f5a1f2622893a5c832cce4ea41
Hardik-KRMU <hardikchandra713@gmail.com>
2026-05-04 22:55:43 +0530

    first commit

--- 07.py
+++ 07.py
@@ -1,17 +1,7 @@
 class Solution:
     def reverse(self, x):
-        res = 0
         sign = -1 if x < 0 else 1
-        x = abs(x)
+        s = str(abs(x))[::-1]
+        res = sign * int(s)
 
-        while x:
-            digit = x % 10
-            x //= 10
-
-            # overflow check (32-bit)
-            if res > (2**31 - 1) // 10:
-                return 0
-
-            res = res * 10 + digit
-
-        return sign * res
+        return res if -2**31 <= res <= 2**31 - 1 else 0
