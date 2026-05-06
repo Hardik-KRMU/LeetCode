c5bdd0a2b504e5db2d50e1b0cf25875e2ae9007b
Hardik-KRMU <hardikchandra713@gmail.com>
2026-05-06 23:32:10 +0530

    first commit

--- 1861.py
+++ 1861.py
@@ -1,5 +1,14 @@
-class Solution:
+cclass Solution:
     def rotateTheBox(self, box):
         m, n = len(box), len(box[0])
 
-       
+        # apply gravity (move stones right)
+        for r in range(m):
+            empty = n - 1
+            for c in range(n - 1, -1, -1):
+                if box[r][c] == '*':
+                    empty = c - 1
+                elif box[r][c] == '#':
+                    box[r][c] = '.'
+                    box[r][empty] = '#'
+                    empty -= 1
