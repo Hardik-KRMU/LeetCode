9d59825bc471d4a33ca058460a451d75002dfdf9
Hardik-KRMU <hardikchandra713@gmail.com>
2026-05-07 23:25:35 +0530

    first commit

--- 3660.py
+++ 3660.py
@@ -1,3 +1,12 @@
 class Solution:
     def maxValue(self, nums):
         n = len(nums)
+
+        # prefix maximums
+        pre = [0] * n
+        pre[0] = nums[0]
+
+        for i in range(1, n):
+            pre[i] = max(pre[i - 1], nums[i])
+
+       
