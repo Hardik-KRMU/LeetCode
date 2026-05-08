c857e5bc2fe5583cabd1a131ac96ddb0be8073de
Hardik-KRMU <hardikchandra713@gmail.com>
2026-05-08 21:55:18 +0530

    first commit

--- 3629.py
+++ 3629.py
@@ -3,3 +3,8 @@ from collections import defaultdict, deque
 class Solution:
     def minJumps(self, nums):
         n = len(nums)
+
+        # check prime
+        def is_prime(x):
+            if x < 2:
+                return False
