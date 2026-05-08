0a7e431786834a0faea8c9e7d8f65c97511f2cd5
Hardik-KRMU <hardikchandra713@gmail.com>
2026-05-08 21:55:38 +0530

    first commit

--- 3629.py
+++ 3629.py
@@ -8,3 +8,18 @@ class Solution:
         def is_prime(x):
             if x < 2:
                 return False
+            i = 2
+            while i * i <= x:
+                if x % i == 0:
+                    return False
+                i += 1
+            return True
+
+        # map: prime -> indices divisible by prime
+        div = defaultdict(list)
+
+        for i, num in enumerate(nums):
+            x = num
+            d = 2
+
+            while d * d <= x:
